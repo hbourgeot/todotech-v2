b362dadce33c3f27c7717f709f7a2ee2c5806501
henrrybourgeot <henrry.bourgeot@linktic.com>
2023-05-16 09:05:46 -0400

    added libraries and new page

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Menu2 } from '@steeze-ui/tabler-icons';
+	import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<!-- Router Slot -->
+
+	<slot />
+
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
-	import type { PageData } from './home/$types';
-
-	export let data: PageData;
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { BrandWhatsapp } from '@steeze-ui/tabler-icons';
 </script>
+
+<main>
+	<header class="hero">
+		<section class="bg-gray-900/50 h-full flex flex-col gap-y-5 w-full justify-center items-center">
+			<h2 class="text-4xl md:text-8xl font-black">Somos Inversiones 7H</h2>
+			<p class="text-xl md:text-2xl font-semibold">Todo a tu alcance.</p>
+			<a
+				target="_blank"
+				href="https://wa.me/584145984147?text=Hola! Quisiera saber un poco más sobre "
+				class="btn bg-white text-green-600 text-lg"
+				><Icon src={BrandWhatsapp} class="h-5 w-5 mr-2" /> Contáctanos</a
+			>
+		</section>
+	</header>
+	<section class="container mx-auto my-5">
+		<h2 class="text-4xl">Conoce nuestros productos</h2>
+	</section>
+</main>
+
+<style lang="scss">
+	.hero {
+		background-image: url(https://cdn.pixabay.com/photo/2023/03/17/16/41/traffic-7859033_960_720.jpg);
+		height: 400px;
+		background-size: cover;
+		color: white;
+	}
+</style>
